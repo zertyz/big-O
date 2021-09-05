2c66ddec03f9157e45c655ef4df12e9c73413bd6
Luiz Silveira <zertyz@gmail.com>
2021-09-05 19:05:04 -0300

    improved space complexity analysis

--- src/big_o_analysis.rs
+++ src/big_o_analysis.rs
@@ -55,8 +55,9 @@ mod tests {
         const TIME_UNIT: &TimeUnit<u128> = &TimeUnits::MICROSECOND;
 
         fn o_1_select(mut _n: u32) -> u32 {
-            // allocations: zero used memory
-            busy_loop(BUSY_LOOP_DELAY*5)
+            // single element allocation & busy_loop time processing
+            let vec = vec![busy_loop(BUSY_LOOP_DELAY*5)];
+            vec.iter().sum()
         }
 
         fn o_log_n_select(mut n: u32) -> u32 {
@@ -66,13 +67,14 @@ mod tests {
             } else {
                 n = PASS_2_SET_SIZE;
             }
-            let mut vec = Vec::<u32>::with_capacity(0);
+            let mut len = 0;
             while n > 0 {
                 r += busy_loop(BUSY_LOOP_DELAY/2);
                 n /= 2;
-                vec.push(n);
+                len += 1;
             }
-            r ^ vec.iter().sum::<u32>()
+            let mut vec = Vec::<u32>::with_capacity(len*400);
+            r ^ (len as u32 + vec.iter().sum::<u32>())
         }
 
         fn o_n_select(mut n: u32) -> u32 {
@@ -82,13 +84,14 @@ mod tests {
             } else {
                 n = PASS_2_SET_SIZE;
             }
-            let mut vec = Vec::<u32>::with_capacity(0);
+            let mut len = 0;
             while n > 0 {
                 r += busy_loop(BUSY_LOOP_DELAY/500);
                 n -= 1;
-                vec.push(n);
+                len += 1;
             }
-            r ^ vec.iter().sum::<u32>()
+            let mut vec = Vec::<u32>::with_capacity(len);
+            r ^ (len as u32 + vec.iter().sum::<u32>())
         }
 
         let assert = |measurement_name, select_function: fn(u32) -> u32, expected_complexity| {
@@ -129,7 +132,7 @@ mod tests {
             };
 
             OUTPUT(&format!("\n{} (r={})\n", algorithm_analysis, r1+r2+r3));
-//            assert_eq!(algorithm_analysis.space_complexity, expected_complexity, "Algorithm SPACE Analysis on CONSTANT SET algorithm for '{}' check failed!", measurement_name);
+            assert_eq!(algorithm_analysis.space_complexity, expected_complexity, "Algorithm SPACE Analysis on CONSTANT SET algorithm for '{}' check failed!", measurement_name);
             assert_eq!(algorithm_analysis.time_complexity,  expected_complexity, "Algorithm TIME  Analysis on CONSTANT SET algorithm for '{}' check failed!", measurement_name);
 
         };
@@ -144,47 +147,48 @@ mod tests {
     #[serial(cpu)]
     fn analyse_set_resizing_algorithm_real_test() {
 
+        const DELTA_SET_SIZE: u32 = 3000;
+
         fn o_1_insert(mut _n: u32) -> u32 {
             // single element allocation & busy_loop time processing
-            let mut vec = vec![busy_loop(BUSY_LOOP_DELAY*2)];
+            let vec = vec![busy_loop(BUSY_LOOP_DELAY*2)];
             vec.iter().sum()
         }
 
         fn o_log_n_insert(mut n: u32) -> u32 {
             let mut r: u32 = 0;
-            let mut len = 0;
+            let mut len = if n==DELTA_SET_SIZE-1 {DELTA_SET_SIZE*2/3} else {0};
             while n > 0 {
                 r = r ^ busy_loop(BUSY_LOOP_DELAY/2);
                 n = n/2;
-                len += BUSY_LOOP_DELAY/2;
+                len += n;
             }
-            let mut vec = Vec::<u32>::with_capacity(len as usize);
-            r ^ vec.iter().sum::<u32>()
-        }
+            let vec = Vec::<u32>::with_capacity(len as usize);
+            r ^ (len + vec.iter().sum::<u32>())
+        };
 
         /// this would be an O(n/2) function -- the average case for a naive sorted insert... but still O(n). Change n = n-2 to n = n-1 and the analysis will be the same.
         fn o_n_insert(mut n: u32) -> u32 {
             let mut r: u32 = 0;
-            let mut vec = Vec::<u32>::with_capacity(0);
+            let len = if n<=DELTA_SET_SIZE {(n/20)*(n/20)} else {(n/20)*(n/20)-(n/40)*(n/40)};
             while n > 1 {
                 r = r ^ busy_loop(BUSY_LOOP_DELAY/50);
                 n = n-2;
-                vec.push(n);
             }
-            r ^ vec.iter().sum::<u32>()
-        }
+            let vec = Vec::<u32>::with_capacity(len as usize * 400);
+            r ^ (len as u32 + vec.iter().sum::<u32>())
+        };
 
         let assert = |measurement_name, insert_function: fn(u32) -> u32, expected_complexity| {
-            let delta_set_size = 3000;
-            OUTPUT(&format!("Real '{}' with {} elements on each pass ", measurement_name, delta_set_size));
+            OUTPUT(&format!("Real '{}' with {} elements on each pass ", measurement_name, DELTA_SET_SIZE));
 
             /* warmup pass -- container / database should be reset before and after this */
-            let (warmup_result, r1) = _run_pass("(warmup: ", "",    insert_function, &BigOAlgorithmType::SetResizing, 0 .. delta_set_size / 10,             &TimeUnits::MICROSECOND);
+            let (warmup_result, r1) = _run_pass("(warmup: ", "", insert_function, &BigOAlgorithmType::SetResizing, 0 .. DELTA_SET_SIZE / 10, &TimeUnits::MICROSECOND);
             /* if we were operating on real data, we would reset the container / database after the warmup, before running pass 1 */
-            let (pass_1_result, r2) = _run_pass("; pass1: ", "",    insert_function, &BigOAlgorithmType::SetResizing, 0 .. delta_set_size,                  &TimeUnits::MICROSECOND);
-            let (pass_2_result, r3) = _run_pass("; pass2: ", "): ", insert_function, &BigOAlgorithmType::SetResizing, delta_set_size .. delta_set_size * 2, &TimeUnits::MICROSECOND);
+            let (pass_1_result, r2) = _run_pass("; pass1: ", "", insert_function, &BigOAlgorithmType::SetResizing, 0 ..DELTA_SET_SIZE, &TimeUnits::MICROSECOND);
+            let (pass_2_result, r3) = _run_pass("; pass2: ", "): ", insert_function, &BigOAlgorithmType::SetResizing, DELTA_SET_SIZE.. DELTA_SET_SIZE * 2, &TimeUnits::MICROSECOND);
 
-            let set_resizing_passes_info = SetResizingAlgorithmPassesInfo { delta_set_size };
+            let set_resizing_passes_info = SetResizingAlgorithmPassesInfo { delta_set_size: DELTA_SET_SIZE };
 
             let time_measurements = BigOTimeMeasurements {
                 pass_1_measurements: pass_1_result.time_measurements,
@@ -211,7 +215,7 @@ mod tests {
             };
             
             OUTPUT(&format!("\n{} (r={})\n", algorithm_analysis, r1^r2^r3));
-//            assert_eq!(algorithm_analysis.space_complexity, expected_complexity, "Algorithm SPACE Analysis on SET RESIZING algorithm for '{}' check failed!", measurement_name);
+            assert_eq!(algorithm_analysis.space_complexity, expected_complexity, "Algorithm SPACE Analysis on SET RESIZING algorithm for '{}' check failed!", measurement_name);
             assert_eq!(algorithm_analysis.time_complexity,  expected_complexity, "Algorithm TIME  Analysis on SET RESIZING algorithm for '{}' check failed!", measurement_name);
         };
 
